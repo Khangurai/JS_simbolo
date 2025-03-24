5fa972acd054e42cb0d1ec031896e22b2278071e
Khangurai <79690068+Khangurai@users.noreply.github.com>
2025-03-24 19:13:16 +0630

    week 3

---
+++ function.js
@@ -0,0 +1,26 @@
+//console.log(sum(13,4)); // hoisting
+
+//fuction declaration
+// function sun(x,y){
+//     return x+y;
+// }
+
+// console.log(add(4,5));
+
+//function expression
+// let z = "hi";
+// let add = function (a,b){
+//     return a+b;
+// };
+
+// console.log(add(4,5));
+
+// //arrow function
+// let double = function (x){
+//     return x*2;
+// };
+
+// console.log(double(5));
+
+let double = (x) => x * 2;
+console.log(double(5));
