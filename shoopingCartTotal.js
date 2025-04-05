19c9d8a0a3e7ed73e679bfbe7c9f0587165eacc7
Khangurai <glorykai7@gmail.com>
2025-04-05 20:53:51 +0630

    Example Case

---
+++ shoopingCartTotal.js
@@ -0,0 +1,10 @@
+const cart = [
+  { item: "Laptop ", price: 1200 },
+  { item: "Headphones ", price: 100 },
+  { item: "Mouse ", price: 50 },
+  { item: "Keyboard ", price: 75 },
+];
+
+let toatalCost = 0;
+
+console.log("Total cost:", toatalCost);
